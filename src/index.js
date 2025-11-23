2672da0242628cec4cedc5cab6bb1554d347bd20
OMM <ommmalhotra02@gmail.com>
2025-11-23 18:02:38 +0530

    App utils and utility wrappers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+import app from "./app.js";
 
 dotenv.config({path: './env'})
 
 connectDB()
+.then(() => {
+    app.on((err) => {
+        console.log("DB connected, error before app listen ", err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server running at port ", process.env.PORT);
+        
+    })
+})
+.catch((err) => {
+    console.log("connectDB called, still connection failed ", err);
+})
 
 // APPROACH 1 - DB connectiona dn app initilization in one file, all on server start
 /*
