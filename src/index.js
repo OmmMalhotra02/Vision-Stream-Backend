39c0ff944490235c7fa86339126c1bf571670793
OMM <ommmalhotra02@gmail.com>
2025-11-26 17:57:23 +0530

    Controller and Routes setup and test using Postman

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
 import app from "./app.js";
 
-dotenv.config({path: './env'})
+dotenv.config({path: './.env'})
 
 connectDB()
 .then(() => {
-    app.on((err) => {
-        console.log("DB connected, error before app listen ", err);
-        throw err;
-    })
+    // app.on((err) => {
+    //     console.log("DB connected, error before app listen ", err);
+    //     throw err;
+    // })
     app.listen(process.env.PORT || 8000, () => {
         console.log("Server running at port ", process.env.PORT);
         
