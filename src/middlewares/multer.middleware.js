486c9966de83cc124726041ea25268186543485d
OMM <ommmalhotra02@gmail.com>
2025-12-16 20:08:48 +0530

    multer upload updated check

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,13 +1,22 @@
 import multer from 'multer'
+import fs from 'fs'
+import path from 'path'
+
+const tempDir = path.join(process.cwd(), 'public', 'temp')
+
+// Make sure temp dir exists
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp")
+    cb(null, tempDir)
   },
   filename: function (req, file, cb) {
-    
-    cb(null, file.originalname)
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
   }
 })
 
-export const upload = multer({ storage: storage })
+export const upload = multer({ storage });
